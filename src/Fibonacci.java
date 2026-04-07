b6919896783071ffd56ba7ed8c7124d7c8b0c9d8
gulnaz kuatova st2506 <254305@astanait.edu.kz>
2026-04-07 08:46:30 +0500

    updated, removed mistakes

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,14 +4,12 @@ public class Fibonacci {
     static int callCount = 0;
 
     public static int fib(int n) {
-        callCount++;
 
         if (n == 0) return 0;
         if (n == 1) return 1;
 
-        return fib(n - 1) + fib(n - 2);
+        return fib(n-1) + fib(n-2);
     }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
